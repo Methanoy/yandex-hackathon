d49994f91dc7871c691c06d38b32a97e8a660dcb
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-11-29 20:32:21 +0300

    refactor: adaptive style middle part of landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,44 @@
 import './index.scss';
+
+
+const selectSingle = document.querySelector('.select');
+const selectSingleTitle = selectSingle.querySelector('.select__title');
+const selectSingleLabels = selectSingle.querySelectorAll('.select__label');
+
+// Toggle position-select menu
+selectSingleTitle.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingleLabels.length; i++) {
+  selectSingleLabels[i].addEventListener('click', (evt) => {
+    selectSingleTitle.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
+// const selectActivity = document.querySelector('.activity-select');
+// const selectActivityTitle = selectSingle.querySelector('.activity-title');
+// const selectActivityLabels = selectSingle.querySelectorAll('.activity-label');
+
+// // Toggle position-select menu
+// selectActivityTitle.addEventListener('click', () => {
+//   if ('active' === selectActivity.getAttribute('activity-data-state')) {
+//     selectActivity.setAttribute('activity-data-state', '');
+//   } else {
+//     selectActivity.setAttribute('activity-data-state', 'active');
+//   }
+// });
+
+// // Close when click to option
+// for (let i = 0; i < selectActivityLabels.length; i++) {
+//   selectActivityLabels[i].addEventListener('click', (evt) => {
+//     selectActivityTitle.textContent = evt.target.textContent;
+//     selectActivity.setAttribute('activity-data-state', '');
+//   });
+// }
