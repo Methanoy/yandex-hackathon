90f44489e33d752c9f68f7aa0f36bd16a39f9e33
artem-from-spb <96229835+artem-from-spb@users.noreply.github.com>
2022-11-30 17:54:47 +0400

    feat: adds figma styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import './index.scss';
 
 
 // Код для слайдера, потом перенесу. Артем
-///////////////////////////////////////////////
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
@@ -10,13 +9,12 @@ const nextButton = document.getElementById("slide-arrow-next");
 
 nextButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
+  const part = slideWidth/4
+  slidesContainer.scrollLeft += part;
 });
 
 prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
+  const part = slideWidth/4
+  slidesContainer.scrollLeft -= part;
 });
-////////////////////////////////////////////
-
-
