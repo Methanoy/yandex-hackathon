066d4e0abb917655511658bc54f7b7ec013c6b8a
artem-from-spb <96229835+artem-from-spb@users.noreply.github.com>
2022-12-02 09:05:29 +0400

    feat: adds mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import './index.scss';
+import "./index.scss";
 
 // Код для выпадающего списка формы
-const selectSingle = document.querySelector('.select');
-const selectSingleTitle = selectSingle.querySelector('.select__title');
-const selectSingleLabels = selectSingle.querySelectorAll('.select__label');
+const selectSingle = document.querySelector(".select");
+const selectSingleTitle = selectSingle.querySelector(".select__title");
+const selectSingleLabels = selectSingle.querySelectorAll(".select__label");
 // Код для слайдера, потом перенесу. Артем
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
@@ -11,19 +11,19 @@ const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
 // Toggle position-select menu
-selectSingleTitle.addEventListener('click', () => {
-  if ('active' === selectSingle.getAttribute('data-state')) {
-    selectSingle.setAttribute('data-state', '');
+selectSingleTitle.addEventListener("click", () => {
+  if ("active" === selectSingle.getAttribute("data-state")) {
+    selectSingle.setAttribute("data-state", "");
   } else {
-    selectSingle.setAttribute('data-state', 'active');
+    selectSingle.setAttribute("data-state", "active");
   }
 });
 
 // Close when click to option
 for (let i = 0; i < selectSingleLabels.length; i++) {
-  selectSingleLabels[i].addEventListener('click', (evt) => {
+  selectSingleLabels[i].addEventListener("click", (evt) => {
     selectSingleTitle.textContent = evt.target.textContent;
-    selectSingle.setAttribute('data-state', '');
+    selectSingle.setAttribute("data-state", "");
   });
 }
 
@@ -48,14 +48,58 @@ for (let i = 0; i < selectSingleLabels.length; i++) {
 //   });
 // }
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  const part = slideWidth/4
-  slidesContainer.scrollLeft += part;
-});
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  const part = slideWidth/4
-  slidesContainer.scrollLeft -= part;
-});
+// Карусель
+
+/* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+// /* Устанавливаем текущий слайд: */
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides = document.getElementsByClassName("item");
+console.log(slides.length)
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides[slideIndex - 1].style.display = "block";
+}
+
+const next = document.querySelector('.next');
+const previous = document.querySelector('.previous');
+
+next.addEventListener('click', () => {
+  nextSlide()
+})
+
+previous.addEventListener('click', () => {
+  previousSlide()
+})
+
